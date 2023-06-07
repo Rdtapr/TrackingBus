cdd35aad79e7c7b6e49944169fbdb5e941c88192
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2023-06-07 15:03:29 +0700

    init

---
+++ router.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const APP_ROUTER_V1 = require("./app/router");
+const ROLE_V1 = require("./api_role/v1/router");
+const USER_V1 = require("./api_user/v1/router");
+const SISWA_V1 = require("./api_supir/v1/router");
+
+router.use("/", APP_ROUTER_V1);
+router.use("/api/v1/role", ROLE_V1);
+router.use("/api/v1/user", USER_V1);
+router.use("/api/v1/supir", SISWA_V1);
+
+module.exports = router;
